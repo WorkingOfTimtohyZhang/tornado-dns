4a8da7d73f02d3daeaf4bff51251ed8a1ad352df
zts1993 <zts1993@gmail.com>
2015-09-28 23:30:02 +0800

    add some notes

--- example.py
+++ example.py
@@ -13,6 +13,9 @@ def main():
         print tornado_dns.errors.describe(code)
         io_loop.stop()
 
+    # use DNS compression pointer mutation
+    # for more info goto https://gist.github.com/klzgrad/f124065c0616022b65e5
+    # use  use_compress=True enable it ~
     tornado_dns.lookup("www.facebook.com", success, errback, timeout=5000, server="8.8.8.8", use_compress=True)
     io_loop.start()
 
